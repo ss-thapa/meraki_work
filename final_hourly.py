f16c79a3390b350ad1c083f029ce22d5ed2ebd5f
ss-thapa <145964403+ss-thapa@users.noreply.github.com>
2024-02-15 22:16:16 +0545

    update

--- final_hourly.py
+++ final_hourly.py
@@ -26,8 +26,8 @@ final_df = df_sales_main[col_name]
 
 final_df['created_date_ad'] = pd.to_datetime(final_df['created_date_ad'])
 
-final_df = final_df[final_df['grand_total'] < 20000]
-final_df = final_df[final_df['grand_total'] != 0]
+# final_df = final_df[final_df['grand_total'] < 20000]
+# final_df = final_df[final_df['grand_total'] != 0]
 
 
 final_df =final_df.rename(columns={'created_date_ad':'ds','grand_total':'y'})
@@ -36,52 +36,39 @@ final_df['ds'] = final_df['ds'].dt.tz_localize(None)
 
 final_df = final_df[final_df['ds'].dt.strftime('%Y-%m-%d') != '2024-02-03']
 
-saturdays = pd.date_range(start=final_df['ds'].min(), end=final_df['ds'].max(), freq='W-SAT')
+final_df['holiday'] = (final_df['ds'].dt.dayofweek == 5).astype(int)
 
-holidays_df = pd.DataFrame({'holiday': 'Saturday', 'ds': saturdays})
-
-final_df =final_df.merge(holidays_df, on='ds', how='left')
-
-final_df['holiday'] =final_df['holiday'].fillna(0)
+final_df = final_df[(final_df['ds'].dt.hour >= 9) & (final_df['ds'].dt.hour <= 20)]
 
 
 
+# print(final_df[final_df['ds'].dt.strftime('%Y-%m-%d') == '2024-01-04'])
 
-mod = Prophet(weekly_seasonality=20,daily_seasonality=3)
+
+mod = Prophet()
 
 mod.add_regressor('holiday')
 
 model = mod.fit(final_df)
 
-# Generate future dates on hourly basis
-future_dates = pd.date_range(start=final_df['ds'].max()+ pd.Timedelta(days=1), periods=24*8, freq='H')  
-future = pd.DataFrame({'ds': future_dates})
+# # Generate future dates on hourly basis
+
+future = model.make_future_dataframe(periods=200, freq='H')
+future['ds'] = pd.to_datetime(future['ds'])
+future = future[(future['ds'] >= pd.Timestamp('2024-02-05 09:00:00')) & (future['ds'] <= pd.Timestamp('2024-02-05 20:00:00'))]
+
 
 future['holiday'] = (future['ds'].dt.dayofweek == 5).astype(int)
 
+
+
+
 forecast = model.predict(future)
 
+print(forecast['yhat'].sum())
 
-
-forecast_2024_02_04 = forecast[(forecast['ds'] >= '2024-02-04 00:00:00') & (forecast['ds'] < '2024-02-05 00:00:00')]
-print(forecast_2024_02_04)
-yhat_predicted_for_day = forecast_2024_02_04['yhat'].sum()
-
-
-
-
-# print(forecast)
-
-# future = model.make_future_dataframe(periods=24*7, freq='h', include_history=False)
-# forecast = model.predict(future)
-
-
-
-
-# forecast_2024_02_04 = forecast[(forecast['ds'] >= '2024-02-05 00:00:00') & (forecast['ds'] < '2024-02-06 00:00:00')]
-# yhat_predicted_for_day = forecast_2024_02_04['yhat'].sum()
-
-# print(forecast_2024_02_04)
+# model.plot(forecast)
+# plt.show()
 
 
 
